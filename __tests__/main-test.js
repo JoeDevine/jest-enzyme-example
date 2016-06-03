626d6858511073469547c6a3d7052cc2fd6035af
Joe Devine <joedevine@uk.ibm.com>
2016-06-03 16:36:24 +0100

    enzyme tests added for shallow, mount and render

--- __tests__/main-test.js
+++ __tests__/main-test.js
@@ -4,13 +4,26 @@ jest
     .unmock('path');
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import Content from '../client/components/content';
 
-describe('Example Project', () => {
-
+describe('Shallow Render of Example Project', () => {
     it('should have an appropriate header', () => {
         const shallowContent = shallow(<Content />);
         expect(shallowContent.contains(<h1>Example Project</h1>));
+    });
+});
+
+describe('Full DOM Render of Example Project', () => {
+    it('should apply appropriate properties', () => {
+        const fullContent = mount(<Content testProp='testProps' />);
+        expect(fullContent.props().testProp).toEqual('testProps');
+    });
+});
+
+describe('Static Rendered Markup of Example Project', () => {
+    it('should have an appropriate header', () => {
+        const staticContent = render(<Content />);
+        expect(staticContent.text()).toContain('Example Project');
     });
 });
